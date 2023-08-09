35989b7965eb5d5360e751d213c2bf3322cad4ba
Louis Ruocco <louisruocco1@gmail.com>
2023-08-09 13:48:31 +0100

    split exercises into strength and cardio arrays, then able to randomise an exercise type and pull out a certain number of exercises relating to that exercise type

--- script.ps1
+++ script.ps1
@@ -1,26 +1,37 @@
 # Add exercises to array
-$exercises = @(
+$strength = @(
     "Pullups", 
     "Bicep Curls"
     "Deadlift", 
     "bench Press (Bar or Dumbbell)", 
-    "1km run",
-    "1km row"
     "Renegade Rows", 
-    "Box Jumps", 
     "Leg Press", 
     "Chest Fly"
     "Lat Pulldown", 
-    "1km Rowing"
     "Goblet Squats", 
-    "Situps", 
+    "Situps",
     "Dumbbell Ab Crunches", 
-    "Leg Raises"
-    "Skull Crushers"
+    "Leg Raises",
+    "Skull Crushers",
     "Tricep Rows"
 )
-# randomise 4-5 exercises
-$workout = $exercises | Sort-Object{Get-Random}
-$workout[0..4]
+
+$cardio = @(
+    "1km run",
+    "Box Jumps",
+    "1km Rowing",
+    "1km bike"
+)
+
+# randomise 4-5 exercises + supersets
+$exercise = "strength", "cardio" | Sort-Object {Get-Random}
+if($exercise[0] -like "strength"){
+    $workout = $strength | Sort-Object{Get-Random}
+    $workout[0..7]
+} else {
+    $workout = $cardio
+    $workout
+}
+
 
 # Send email with randomised exercises on it
